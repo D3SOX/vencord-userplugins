a567e803fcc44368509603395699262c93700aec
D3SOX <nico@d3sox.me>
2024-05-06 18:34:52 +0200

    chore(betterActivities): cleanup unused code

--- betterActivities/index.tsx
+++ betterActivities/index.tsx
@@ -85,15 +85,6 @@ const TimeBar = findComponentByCodeLazy<{
     className: string;
 }>("isSingleLine");
 
-const ActivityComponent = findComponentByCodeLazy<{
-    activity: Activity;
-    application?: Application;
-    user: User;
-    channelId?: string;
-}>("get activity");
-
-type OverlayType = { default: { Overlay: React.ComponentType<{ children?: React.ReactNode; }>; }; };
-
 // if discord one day decides to change their icon this needs to be updated
 const DefaultActivityIcon = findComponentByCodeLazy("M6,7 L2,7 L2,6 L6,6 L6,7 Z M8,5 L2,5 L2,4 L8,4 L8,5 Z M8,3 L2,3 L2,2 L8,2 L8,3 Z M8.88888889,0 L1.11111111,0 C0.494444444,0 0,0.494444444 0,1.11111111 L0,8.88888889 C0,9.50253861 0.497461389,10 1.11111111,10 L8.88888889,10 C9.50253861,10 10,9.50253861 10,8.88888889 L10,1.11111111 C10,0.494444444 9.5,0 8.88888889,0 Z");
 
@@ -352,14 +343,6 @@ export default definePlugin({
         }
 
         return null;
-    },
-
-    getActivityComponent: (activity: Activity, user: User, channel: Channel, overlay: OverlayType): JSX.Element | null => {
-        console.log("getActivityComponent", activity, user, channel);
-        const OverlayComponent: React.ComponentType<React.PropsWithChildren> = overlay.default.Overlay;
-        return <OverlayComponent>
-            <ActivityComponent activity={activity} user={user} channelId={channel.id} />
-        </OverlayComponent>;
     },
 
     showAllActivitiesComponent({ activity, user, guild, channelId, onClose }: { activity: Activity; user: User, guild: Guild, channelId: string, onClose: () => void; }) {
