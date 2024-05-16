3007aad58c358d8bc3f72470875b0e959e13d8ad
D3SOX <nico@d3sox.me>
2024-05-16 23:50:27 +0200

    fix(noDefaultHangStatus): forget all that type magic that broke it, just ignore

--- noDefaultHangStatus/index.ts
+++ noDefaultHangStatus/index.ts
@@ -5,23 +5,10 @@
  */
 
 import { Devs } from "@utils/constants";
-import definePlugin, { PluginDef } from "@utils/types";
+import definePlugin from "@utils/types";
 import { FluxDispatcher } from "@webpack/common";
-import { FluxEvents } from "@webpack/types";
 
-type MyPluginDef = PluginDef & {
-    flux: {
-        UPDATE_HANG_STATUS: ({ status, saveAsDefault }: { status: string, saveAsDefault?: boolean }) => void;
-    };
-};
-
-type ExtendedFluxDispatcherType = typeof FluxDispatcher & {
-    // Extend the dispatch method to accept the extended event types
-    dispatch(event: { [key: string]: unknown; type: FluxEvents | "CLEAR_HANG_STATUS"; }): Promise<void>;
-}
-
-const ExtendedFluxDispatcher = FluxDispatcher as unknown as ExtendedFluxDispatcherType;
-
+// @ts-ignore
 export default definePlugin({
     name: "NoDefaultHangStatus",
     description: "Disable the default hang status when joining voice channels",
@@ -30,8 +17,9 @@ export default definePlugin({
     flux: {
         UPDATE_HANG_STATUS: ({ status, saveAsDefault }) => {
             if (saveAsDefault === undefined && status) {
-                ExtendedFluxDispatcher.dispatch({ type: "CLEAR_HANG_STATUS" });
+                // @ts-ignore
+                FluxDispatcher.dispatch({ type: "CLEAR_HANG_STATUS" });
             }
         },
     }
-} as MyPluginDef);
+});
